9d7b284ef7b2dd22d87a554dc60668ef8cca6107
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-07-05 22:37:23 -0300

    fixes

--- src/flext_target_ldap/__init__.py
+++ src/flext_target_ldap/__init__.py
@@ -19,7 +19,7 @@ from flext_target_ldap.__version__ import (
 from flext_target_ldap._exports import FLEXT_TARGET_LDAP_LAZY_IMPORTS
 
 if TYPE_CHECKING:
-    from flext_core._root_typing_parts import (
+    from flext_core._root_typing_parts.facades import (
         d as d,
         e as e,
         h as h,
--- src/flext_target_ldap/_exports.py
+++ src/flext_target_ldap/_exports.py
@@ -3,14 +3,50 @@
 
 from __future__ import annotations
 
-from flext_core.lazy import merge_lazy_imports
-from flext_target_ldap._exports_lazy_part_01 import (
-    FLEXT_TARGET_LDAP_LAZY_IMPORTS_PART_01,
-)
+from flext_core.lazy import build_lazy_import_map, merge_lazy_imports
 
-_LOCAL_LAZY_IMPORTS = {
-    **FLEXT_TARGET_LDAP_LAZY_IMPORTS_PART_01,
-}
+_LOCAL_LAZY_IMPORTS = build_lazy_import_map(
+    {
+        "._constants": ("_constants",),
+        "._models": ("_models",),
+        "._utilities": ("_utilities",),
+        ".api": (
+            "FlextTargetLdap",
+            "target_ldap",
+        ),
+        ".application": ("application",),
+        ".application.orchestrator": ("FlextTargetLdapOrchestrator",),
+        ".constants": (
+            "FlextTargetLdapConstants",
+            "c",
+        ),
+        ".models": (
+            "FlextTargetLdapModels",
+            "m",
+        ),
+        ".protocols": (
+            "FlextTargetLdapProtocols",
+            "p",
+        ),
+        ".settings": ("FlextTargetLdapSettings",),
+        ".typings": (
+            "FlextTargetLdapTypes",
+            "t",
+        ),
+        ".utilities": (
+            "FlextTargetLdapUtilities",
+            "u",
+        ),
+        "flext_core._root_typing_parts.facades": (
+            "d",
+            "e",
+            "h",
+            "r",
+            "s",
+            "x",
+        ),
+    },
+)
 
 FLEXT_TARGET_LDAP_LAZY_IMPORTS = merge_lazy_imports(
     (".application",),
