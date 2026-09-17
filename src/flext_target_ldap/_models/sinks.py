eec17dabe297faa9a27fede2201f9c94a9effa7b
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-17 18:29:11 -0300

    fixes

--- src/flext_target_ldap/_models/sinks.py
+++ src/flext_target_ldap/_models/sinks.py
@@ -30,7 +30,7 @@ class FlextTargetLdapModels:
 
         def __init__(
             self,
-            target: FlextTargetLdapTarget,
+            target: FlextTargetLdapModels.FlextTargetLdapTarget,
             stream_name: str,
             schema: t.TargetLdap.SchemaPayload,
             key_properties: t.StrSequence,
@@ -95,7 +95,7 @@ class FlextTargetLdapModels:
         @override
         def __init__(
             self,
-            target: FlextTargetLdapTarget,
+            target: FlextTargetLdapModels.FlextTargetLdapTarget,
             stream_name: str,
             schema: t.TargetLdap.SchemaPayload,
             key_properties: t.StrSequence,
@@ -104,9 +104,7 @@ class FlextTargetLdapModels:
             super().__init__(target, stream_name, schema, key_properties)
             self._target = target
             self.client: FlextTargetLdapClient | None = None
-            self._processing_result: FlextTargetLdapProcessingResult = (
-                FlextTargetLdapProcessingResult()
-            )
+            self._processing_result: FlextTargetLdapModels.FlextTargetLdapProcessingResult = FlextTargetLdapProcessingResult()
 
         def _apply_attribute_mapping(
             self,
