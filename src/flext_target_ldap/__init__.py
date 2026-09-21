4f1440f9258ca67321016963ea2e65952a68653e
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-21 14:40:34 -0300

    chore(gen): converge projections at the integration tip (fleet fixed point)

--- src/flext_target_ldap/__init__.py
+++ src/flext_target_ldap/__init__.py
@@ -20,9 +20,8 @@ from .__version__ import (
 )
 
 if TYPE_CHECKING:
-    from flext_ldap import s
-
     from flext_core import d, e, h, r, x
+    from flext_ldap import s
 
     from . import application
     from .__version__ import FlextTargetLdapVersion
