2f8ee42f8277802267b529f0d60fc19c1fbed958
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-21 22:26:00 -0300

    fixes

--- src/flext_target_ldap/__init__.py
+++ src/flext_target_ldap/__init__.py
@@ -20,7 +20,9 @@ from .__version__ import (
 )
 
 if TYPE_CHECKING:
-    from flext_ldap import d, e, h, r, s, x
+    from flext_ldap import s
+
+    from flext_core import d, e, h, r, x
 
     from . import application
     from .__version__ import FlextTargetLdapVersion
@@ -86,7 +88,8 @@ _LAZY_IMPORTS = MappingProxyType(
             ".target": ("main",),
             ".typings": ("FlextTargetLdapTypes", "t"),
             ".utilities": ("FlextTargetLdapUtilities", "u"),
-            "flext_ldap": ("d", "e", "h", "r", "s", "x"),
+            "flext_core": ("d", "e", "h", "r", "x"),
+            "flext_ldap": ("s",),
         }),
         alias_groups=MappingProxyType({}),
         sort_keys=False,
